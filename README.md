7c586842afe4e66511abf5d3974ac6ed91c0f36d
Chuyang Liu <chuyang.s.liu@gmail.com>
2017-05-17 20:55:24 +0800

    Init commit

---
+++ README.md
@@ -0,0 +1,49 @@
+# Tools
+
+Tools for preprocessing training/testing data.
+
+## 数据格式
+
+`data` 目录下的每个目录表示一个数据集，每个数据集包含如下几个文件：
+
+```
+data/
+  some-data-set/
+    frames      // 帧图片目录
+    pos.csv     // 录制时站立位置
+    sensor.csv  // 每一帧的传感器数据
+```
+
+## 工具包
+
+### 提取数据集中的部分数据
+
+用法:
+
+```
+python3 select_frames.py <数据集目录名> <起始帧序号> <结束帧序号>
+```
+
+示例:
+
+```
+# 提取shops数据集的第2、3帧以及对应的传感器数据
+python3 select_frames.py shops 2 3
+```
+
+运行成功后将生成 `shops-selected` 目录，包含如下几个文件：
+
+```
+data/
+  shops-selected/
+    frames      // 帧图片目录
+    pos.csv     // 录制时站立位置
+    sensor.csv  // 每一帧的传感器数据
+    config.txt  // 配置文件，存放数据集中帧的个数
+```
+
+`frames` 和 `config.txt` 可以作为[视频跟踪算法](https://github.com/GetYourLocation/KCFcpp)的输入。
+
+## License
+
+See the [LICENSE](./LICENSE) file for license rights and limitations.
---
+++ select_frames.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import shutil
+import time
+
+try:
+    ori_dir = os.path.join("data", sys.argv[1])
+    res_dir = os.path.join("data", sys.argv[1] + "-selected")
+    beg_frame = int(sys.argv[2])
+    end_frame = int(sys.argv[3])
+except Exception as e:
+    print("Usage: python3 select_frames.py" +
+          " <directory name> <begin frame> <end frame>")
+    sys.exit(0)
+
+DIR_FRAMES = "frames"
+FILENAME_POS = "pos.csv"
+FILENAME_SENSOR = "sensor.csv"
+FILENAME_CONFIG = "config.txt"
+
+print("Creating result directory...")
+shutil.rmtree(res_dir, ignore_errors=True)
+time.sleep(0.2)
+os.makedirs(os.path.join(res_dir, DIR_FRAMES))
+
+print("Reading '%s' directory..." % DIR_FRAMES)
+frames = os.listdir(os.path.join(ori_dir, DIR_FRAMES))
+frames = list(map(lambda filename: int(filename[0:filename.find('.')]),
+                  frames))
+frames.sort()
+
+print("Reading %s..." % FILENAME_SENSOR)
+ori_sensor_file = open(os.path.join(ori_dir, FILENAME_SENSOR), 'r')
+ori_sensor_data = ori_sensor_file.readlines()
+ori_sensor_file.close()
+
+print("Selecting frames...")
+res_sensor_data = [ori_sensor_data[0]]
+frame_cnt = 1
+i = frames.index(beg_frame)
+while i < len(frames):
+    if (frames[i] >= beg_frame and frames[i] <= end_frame):
+        shutil.copyfile(
+            os.path.join(ori_dir, DIR_FRAMES, "%d.jpg" % frames[i]),
+            os.path.join(res_dir, DIR_FRAMES, "%d.jpg" % frame_cnt))
+        sensor_row = ori_sensor_data[frames[i]]
+        sensor_row = str(frame_cnt) + sensor_row[int(sensor_row.find(',')):]
+        res_sensor_data.append(sensor_row)
+        frame_cnt += 1
+    i += 1
+
+print("Selecting sensor data...")
+res_sensor_file = open(os.path.join(res_dir, FILENAME_SENSOR), 'w')
+for data in res_sensor_data:
+    res_sensor_file.write(data)
+res_sensor_file.close()
+
+print("Copying %s..." % FILENAME_POS)
+shutil.copyfile(os.path.join(ori_dir, FILENAME_POS),
+                os.path.join(res_dir, FILENAME_POS))
+
+print("Generating %s..." % FILENAME_CONFIG)
+config_file = open(os.path.join(res_dir, FILENAME_CONFIG), 'w')
+config_file.write(str(len(res_sensor_data) - 1))
+config_file.close()
+
+print("Results have been written to '%s'." % res_dir)
